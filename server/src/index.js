6f20db6a3af2c42d949d7fe4115958561389474d
Victor Hugo <victor.aquino@ccc.ufcg.edu.br>
2021-07-10 23:27:04 -0300

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,30 @@
+const Koa = require('koa');
+const http = require('http');
+const socket = require('socket.io');
+
+const app = new Koa();
+const server = http.createServer(app.callback());
+const io = socket(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+const SERVER_PORT = 8080;
+const SERVER_HOST = 'localhost';
+
+io.on('connection', socket => {
+    console.log('[IO] Server has a new connection');
+
+    socket.on('chat.message', (data) => {
+        io.emit('chat.message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('[IO] A connection was disconnected');
+    });
+})
+
+server.listen(SERVER_PORT, SERVER_HOST, () => {
+    console.log("Server Active in port " + SERVER_PORT);
+});
